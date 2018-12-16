7df72625c17795c5e809f3981f11b274b42a1b60
Pieter-Jan Rotsaert <pieterjan.rotsaert@hotmail.com>
2018-12-16 01:01:38 +0100

    Stijl aangepast

---
+++ PrettigLokaal/PrettigLokaal/ViewModels/Helpers/LineConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace PrettigLokaal.ViewModels.Helpers
+{
+    class LineConverter : IValueConverter 
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string str = (string)value;
+            return str.Replace('\n', ' ').Replace('\r', ' ');
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
